47be975acdd5fc9987f2b5b6bc204fddbb0f44e8
PiuSen <peusen113@gmail.com>
2023-07-28 20:30:45 +0530

    update

--- app/src/main/java/com/example/calculator/Splashscreen.kt
+++ app/src/main/java/com/example/calculator/Splashscreen.kt
@@ -1,17 +1,17 @@
-
 package com.example.calculator
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
-        Handler().postDelayed(Runnable {
-                                       startActivity(Intent(this,MainActivity::class.java))
-        },2000)
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
     }
 }
